bbf111b79b9a94df2ea00165f74422b36f2741d2
EdwinSch <edwinschutjes@gmail.com>
2024-01-17 16:03:51 +0100

    add: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
       <div className="decorative"></div>
-      <main>content</main>
+      <main>
+        <Header />
+      </main>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { PiStarFourFill } from "react-icons/pi";
+
+const Header = () => {
+  return (
+    <header>
+      <PiStarFourFill style={{ color: "#ad28eb", fontSize: 38 }} />
+      <h1>FAQs</h1>
+    </header>
+  );
+};
+
+export default Header;
