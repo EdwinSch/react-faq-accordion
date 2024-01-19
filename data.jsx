57b5c1aca781d0d1dcb3d7652f00b4e185d2226c
EdwinSch <edwinschutjes@gmail.com>
2024-01-19 10:26:18 +0100

    debug

--- data.jsx
+++ data.jsx
@@ -1,7 +1,7 @@
 const faq = [
   {
     id: 1,
-    question: "Kickstarter hot chicken try-hard?",
+    question: "Kickstarter hoodie try-hard. Paleo hot chicken?",
     answer:
       "Cold-pressed mustache selvage, blog copper mug jianbing blue bottle. Chillwave ennui gatekeep truffaut ugh. Intelligentsia kinfolk helvetica kale chips, man braid meggings coloring book ethical bushwick synth cornhole.",
   },
@@ -9,11 +9,12 @@ const faq = [
     id: 2,
     question: "Keffiyeh freegan helvetica?",
     answer:
-      "Adaptogen godard 3 wolf moon scenester fam polaroid hashtag chambray vibecession farm-to-table photo booth. Tousled helvetica coloring book wayfarers. Grailed blackbird spyplane literally squid blog post-ironic umami chambray sus roof party yuccie everyday carry.",
+      "Adaptogen godard 3 wolf moon scenester fam polaroid hashtag chambray vibecession farm-to-table photo booth. Tousled helvetica coloring book wayfarers. Grailed blackbird spyplane literally squid blog post-ironic?",
   },
   {
     id: 3,
-    question: "Lo-fi poke synth bodega boys subway tile DSA?",
+    question:
+      "Lo-fi poke synth bodega boys subway tile DSA. Polaroid you probably haven't heard of them put a bird on it chillwave knausgaard?",
     answer:
       "Mustache farm-to-table helvetica schlitz lomo leggings, gochujang hot chicken migas. Meditation deep v locavore jianbing twee hashtag. Helvetica photo booth typewriter meh. Authentic wayfarers mixtape intelligentsia, small batch typewriter freegan fashion axe bespoke microdosing tbh celiac.",
   },
